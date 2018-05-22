235431bd2bd2b9f0d73874928c324e5324aab56b
Rafael Gaino <rgaino@gmail.com>
2018-05-22 20:35:51 -0300

    Implementation of CoreML model InceptionV3 from developer.apple.com/machine-learning

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            guard let ciImage = CIImage(image: image) else {
+                fatalError()
+            }
+            detectImage(image: ciImage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil )
+    }
+    
+    func detectImage(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Fatal error loading CoreML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Couldn't get observations")
+            }
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hot Dog"
+                } else {
+                    self.navigationItem.title = "Not Hot Dog"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        try! handler.perform([request])
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraTApped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated:true, completion: nil)
+        
     }
-
-
+    
 }
 
